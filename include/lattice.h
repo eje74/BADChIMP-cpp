1aec8aa19e154271a4aabc019ccecfcd12977c65
eje <eje@iris.no>
2018-11-12 13:38:33 +0100

    Last commit before code restructure.

---
+++ include/lattice.h
@@ -0,0 +1,9 @@
+#ifndef LATTICE_H
+#define LATTICE_H
+
+/*! LATTICE class
+ *  Contains all information about the basis vectors, and operators for vectors and tensors
+ *  like scalarproducs.
+ */
+
+#endif // LATTICE_H

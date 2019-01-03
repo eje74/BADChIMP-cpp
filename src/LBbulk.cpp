796c398892ee98f72832bdd2963fc4018cc590d3
olau <olau@norceresearch.no>
2019-01-03 09:11:07 +0100

    updated bulk.h and bulk.cpp

--- src/LBbulk.cpp
+++ src/LBbulk.cpp
@@ -4,6 +4,7 @@
 Bulk::Bulk(int nBulkNodes)
 {
   nBulkNodes_=nBulkNodes;
+  nAddedNodes_ = 0;
   bulkNode_ = new int[nBulkNodes_];
 }
 
@@ -11,3 +12,16 @@ Bulk::~Bulk()
 {
   delete [] bulkNode_;
 }
+
+void Bulk::addBulkNode( int nodeNo )
+{
+  if (nAddedNodes_ < nBulkNodes_) {
+        bulkNode_[nAddedNodes_] = nodeNo;
+    }
+    else {
+        std::cout << "ERROR: Added to many boundary nodes!" << std::endl;
+        exit(1);
+    }
+  nAddedNodes_ += 1;
+}
+

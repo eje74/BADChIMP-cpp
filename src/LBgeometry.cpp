f988527ee9b4be43c27786ced2ddc29fac8279a7
eje <eje@iris.no>
2019-01-15 15:52:12 +0100

    Added comments for the geometry functions, and the create and delete label functions. Changed the makeX functions to newX functions and added a fillGeometry function.

--- src/LBgeometry.cpp
+++ src/LBgeometry.cpp
@@ -4,23 +4,80 @@
 #include <assert.h>
 
 
-/* We need to specify for which nodes that we need to allocate memory, and
-   for which nodes we need to get the link information.
-
-void setupGridNeig(const int nLinks, const int* links,  Grid &grid)
+/* ****************************************************
+ * Functions for creating, filling and deleting a 2d
+ * structured GEOMETRY matrix
+ * ****************************************************/
+void newGeometry(const int nX, const int nY, int** &geo)
+/* geo : is a pointer variable
+ *       the value of geo at point (x,y) is geo[y][x]
+ * nX, and nY are the spatial dimensions */
 {
-    for (int n = 0; n < nLinks; ++n)
-    {
-        int num = 3 * n;
-        //               direction    from node       to node
-        grid.addNeigNode(links[num], links[num + 1], links[num + 2]);
-    }
-} */
+    geo = new int* [nY];
+    for (int y = 0; y < nY; ++y)
+        geo[y] = new int [nX];
+}
 
 
+void deleteGeometry(const int nX, const int nY, int** &geo)
+{
+    for (int y = 0; y < nY; ++y)
+        delete []  geo[y];
+    delete [] geo;
+    geo = nullptr;
+}
+
+void inputGeometry(int nX, int nY, int** &geo)
+/*  Makes a geometry:
+ *  The conventions is that:
+ *    FLUID = 0
+ *    SOLID = 1
+ * This function could be replaced by a geo file
+ */
+{
+    for (int y = 0; y < nY; ++y)
+        for (int x = 0; x < nX; ++x)
+        {
+            if (y == 0)
+                geo[y][x] = 1;
+            else
+                geo[y][x] = 0;
+        }
+}
+/*======================================================*/
 
 
-void makeNodeLabel(int nX, int nY, int** &nodeLabel)
+/*******************************************************
+ * Node LABEL functions. Each node in the computational
+ * domains should be given its unique label-tag (int number)
+ *
+ *  Example:
+ *  geo-matrix:     label-matrix:
+ *    4 4 4            0  0  0
+ *    3 3 3           10 11 12
+ *    1 1 1            1  2  3
+ *    0 0 0            4  5  6
+ *    1 1 1            7  8  9
+ *    3 3 3           13 14 15
+ *
+ *  Here we have made the choices that
+ *  - 0 is a default bulk solid tag. We will allocate
+ *    memory for it, but we assume that it contains
+ *    only dummy values
+ *  - We also want the bulk node values to be numbered
+ *    consequently from 1 (number one). In this example
+ *    both fluid bulk and fluid boundary are treated as
+ *    bulk nodes in the LB iterations for macroscopic
+ *    varaible evaluation, collision and propagation.
+ *
+ *******************************************************/
+void newNodeLabel(int nX, int nY, int** &nodeLabel)
+/* nodeLabel is the tag-matrix.
+ *  the tag for the node at point (x,y) is nodeLabel[y][x]
+ * nX and nY is the matrix size
+ *
+ * The matrix is initiated to zero.
+ */
 {
     nodeLabel = new int* [nY];
     for (int y = 0; y < nY; ++y)
@@ -42,6 +99,8 @@ void deleteNodeLabel(int nX, int nY, int** &nodeLabel)
 
 }
 
+
+// EJE START HER
 
 /* We will number bulk labels from 1 to "#of bulk labels" */
 int setBulkLabel(int nX, int nY, int** geo, int** &nodeLabel)
@@ -76,33 +135,6 @@ int setNonBulkLabel(int firstLabel, int nX, int nY, int** geo, int** &nodeLabel)
 
 
 
-void makeGeometry(const int nX, const int nY, int** &geo)
-{
-    geo = new int* [nY];
-    for (int y = 0; y < nY; ++y)
-        geo[y] = new int [nX];
-
-    // MAKE GEOMETRY
-    // FLUID = 0
-    // SOLID = 1
-    for (int y = 0; y < nY; ++y)
-        for (int x = 0; x < nX; ++x)
-        {
-            if (y == 0)
-                geo[y][x] = 1;
-            else
-                geo[y][x] = 0;
-        }
-}
-
-
-void deleteGeometry(const int nX, const int nY, int** &geo)
-{
-    for (int y = 0; y < nY; ++y)
-        delete []  geo[y];
-    delete [] geo;
-    geo = nullptr;
-}
 
 
 void printGeoScr(const int nX, const int nY, int** geo)

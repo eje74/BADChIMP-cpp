01963bfe12dc9770e7096f2ea9a2198d2fc9d8c7
Jan Ludvig @ MBP13 <jlvi@iris.no>
2019-02-06 16:11:13 +0100

    Added code from E&O

---
+++ src/defines.h
@@ -0,0 +1,17 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define FLUID	    0
+#define WALL 	    1
+#define INLET       2
+#define OUTLET      3
+#define SOLID	    4
+#define PERIODIC    5
+#define GHOST       10
+#define DISCARD     11
+#define NUM_MODE    12
+
+#define OUTPUT_DTYPE  float
+
+#endif // DEFINES_H
+

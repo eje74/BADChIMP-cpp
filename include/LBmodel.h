c5d4072a65d2bd4e0ef59da609fa1fba3d36f72f
eje <eje@iris.no>
2019-02-22 15:10:00 +0100

    Added the basic model class.

---
+++ include/LBmodel.h
@@ -0,0 +1,56 @@
+#ifndef LBMODEL_H
+#define LBMODEL_H
+
+#include "LBglobal.h"
+#include "LBfield.h"
+
+/*
+            const int nodeNo = bulk.nodeNo(bulkNo); // Find current node number
+            // UPDATE MACROSCOPIC DENSITIES
+            lbBase_t rho0Node, rho1Node;
+            // Calculate rho for each phase
+            calcRho<LT>(&f(0,0,nodeNo), rho0Node);  // LBmacroscopic
+            rho(0, nodeNo) = rho0Node; // save to global field
+            calcRho<LT>(&f(1,0,nodeNo), rho1Node);  // LBmacroscopic
+            rho(1, nodeNo) = rho1Node; // save to global field
+
+            // Calculate color gradient kernel
+            cgField(0, nodeNo) = (rho0Node - rho1Node)/(rho0Node + rho1Node);
+*/
+
+/*
+    LbField<LT> f(2, nNodes);  // LBfield
+    LbField<LT> fTmp(2, nNodes);  // LBfield
+
+    // SETUP MACROSCOPIC FIELDS
+    ScalarField rho(2, nNodes); // LBfield
+    VectorField<LT> vel(1, nNodes); // LBfield
+    ScalarField cgField(1, nNodes); // LBfield
+
+ */
+
+template <typename LT>
+class TwoPhaseCG
+{
+public:
+    TwoPhaseCG(int nNodes):
+        rho(ScalarField(2, nNodes)),
+        vel(VectorField<LT>(1, nNodes)),
+        f(LbField<LT>(2, nNodes)),
+        fTmp(LbField<LT>(2, nNodes)),
+        cgField(ScalarField(1, nNodes))
+    {
+    }
+    // SETUP MACROSCOPIC FIELDS
+    ScalarField rho; //(2, nNodes); // LBfield
+    VectorField<LT> vel; //(1, nNodes); // LBfield
+
+private:
+    LbField<LT> f; //(2, nNodes);  // LBfield
+    LbField<LT> fTmp; //(2, nNodes);  // LBfield
+    ScalarField cgField; //(1, nNodes); // LBfield
+
+    lbBase_t rho0Node, rho1Node;
+};
+
+#endif // LBMODEL_H

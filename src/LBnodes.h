0efc7018acc3c16e2f8a50011a98f4c4dc766401
eje <eje@iris.no>
2019-05-29 15:50:43 +0200

    Updating the nodes setup function.

--- src/LBnodes.h
+++ src/LBnodes.h
@@ -21,21 +21,36 @@ template<typename DXQY>
 class Nodes
 {
 public:
-    Nodes(const int nNodes): nNodes_(nNodes), nodeType_(static_cast<std::size_t>(nNodes)) {}
+    Nodes(const int nNodes, const int myRank):
+        nNodes_(nNodes),
+        myRank_(myRank),
+        nodeType_(static_cast<std::size_t>(nNodes)),
+        nodeRank_(static_cast<std::size_t>(nNodes), -1),
+        nodeT_(static_cast<std::size_t>(nNodes))
+    {}
     inline int size() const {return nNodes_;}
     void setup(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs);
     inline int getType(const int nodeNo) const {return nodeType_[nodeNo];}
     inline int getRank(const int nodeNo) const {return nodeType_[nodeNo]-1;}
-    static Nodes<DXQY> makeObject(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs, const Grid<DXQY> &grid);
+    //inline int getRank(const int nodeNo) const {return nodeRank_[nodeNo];}
+    static Nodes<DXQY> makeObject(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs, const int myRank, const Grid<DXQY> &grid);
 
 private:
     int nNodes_;
+    int myRank_;
     std::vector<int> nodeType_; // Zero for solid and rank = value-1
+    std::vector<int> nodeRank_; // The actual node type
+    std::vector<short int> nodeT_; // The actual node type
+
 };
 
 
 template<typename DXQY>
 void Nodes<DXQY>::setup(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs)
+/*
+ * mfs : local node number file
+ * rfs : rank number file
+ */
 {
     mfs.reset();
     rfs.reset();
@@ -44,6 +59,13 @@ void Nodes<DXQY>::setup(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs)
     {
         auto nodeNo = mfs.template getVal<int>(); // Gets node number. The template name is needed
         auto nodeType =  rfs.template getVal<int>(); // get the node type
+
+        // If rank is already set to myRank do not change it
+        if (nodeNo > 0)
+            if (nodeRank_[nodeNo] != myRank_)
+                nodeRank_[nodeNo] = nodeType - 1;
+
+
         if ( mfs.insideDomain(pos) ) { // Update the grid object
             if (nodeNo > 0) { // Only do changes if it is a non-default node
                 // Add node type
@@ -56,7 +78,7 @@ void Nodes<DXQY>::setup(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs)
 }
 
 template<typename DXQY>
-Nodes<DXQY> Nodes<DXQY>::makeObject(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs, const Grid<DXQY> &grid)
+Nodes<DXQY> Nodes<DXQY>::makeObject(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs, const int myRank, const Grid<DXQY> &grid)
 /* Makes a grid object using the information in the file created by our
  * python program, for each mpi-processor.
  *
@@ -71,7 +93,7 @@ Nodes<DXQY> Nodes<DXQY>::makeObject(MpiFile<DXQY> &mfs, MpiFile<DXQY> &rfs, cons
  *  4) List of all nodes including rim-nodes for this processor.
  */
 {
-    Nodes<DXQY> newNodes(grid.size());
+    Nodes<DXQY> newNodes(grid.size(), myRank);
 
     mfs.reset();
     rfs.reset();

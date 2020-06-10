Corrected errors in LBfreeSlipCartesian.h. Program compiles, but get numerical artifacts when using free slip boundary condition.

PythonScripts/writeGeoFile.py
@@ -39,12 +39,12 @@ nz = 3
 
 nz = 1
 ny = 300
-nx = 200
+nx = 201
 
 geo = np.zeros([nz, ny, nx])
 
 geo[:,0,:] = 1
-#geo[:,-1,:] = 1
+geo[:,-1,:] = 1
 
 #write_dir = "/home/ejette/Programs/GitHub/BADChIMP-cpp/PythonScripts/"  # Home
 #write_file = "walls.dat"
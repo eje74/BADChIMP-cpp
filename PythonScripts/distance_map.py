220590fc9db0020c30534a99aeaf07b15c2b9d9d
javi@osl-1011 <javi@norceresearch.no>
2022-05-12 16:49:17 +0200

    Blood script updates

--- PythonScripts/distance_map.py
+++ PythonScripts/distance_map.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from pyvista import read as pvread, UniformGrid, PolyData
-from numpy import array, ceil, sum as npsum, ones, double as npdouble, mean
+from numpy import argsort, array, ceil, min as npmin, max as npmax, nonzero, sum as npsum, ones, double as npdouble, mean, zeros, sum as npsum
 from scipy.spatial import KDTree
 from pathlib import Path
 import matplotlib.pyplot as pl
@@ -80,17 +80,56 @@ cl = pvread(clpath)
 #length = []
 #cl_points = cl.points.astype(npdouble)
 ugrid_cells = ugrid.cell_centers().points.astype(npdouble)
+tubes = ones((cl.n_cells, ugrid.n_cells), dtype=int)
+slices = ones((cl.n_cells, ugrid.n_cells), dtype=int)
+length2 = zeros(cl.n_cells)
 for n in range(cl.n_cells):
     cl_points = cl.cell_points(n).astype(npdouble)
+    length2[n] = npsum( npsum((cl_points[1:,:]-cl_points[:-1,:])**2, axis=1) )
     dist_idx = array(KDTree(cl_points).query(ugrid_cells, workers=workers))
     idx = dist_idx[1].astype(int) # centerline point index
-    ugrid[f'line_{n}_distance'] = dist_idx[0]
-    pl.figure(n)
-    m = mean(dist_idx[0])
-    pl.hist(dist_idx[0], 100)
-    print(f'line {n}: mean = {m}')
-    pl.draw()
-pl.show()
+    dist = dist_idx[0]
+    ugrid[f'line_{n}_distance'] = dist
+    m = mean(dist)
+    remove = dist>m
+    tubes[n,:] = n+1
+    tubes[n,remove] = 0
+    #ugrid[f'tube_{n}'] = tubes[n,:]
+    ugrid[f'line_{n}_idx'] = idx #*(tubes[n,:]>0)
+    ugrid[f'line_{n}_idx'][remove] = 0
+    slices[n,:] = idx #*(tubes[n,:]>0)
+    slices[n,:][remove] = 0
+    #pl.figure(n)
+    #pl.hist(dist_idx[0], 100)
+    #print(f'line {n}: mean = {m}')
+    #pl.draw()
+#pl.show()
+
+is_tube = tubes > 0
+sizes = npsum(is_tube, axis=1)
+#print(f'sizes: {sizes}')
+#print(f'length2: {length2}')
+#tube_list = argsort(length2)
+tube_list = [2, 3, 5, 1]
+print(f'tube_list: {tube_list}')
+#tube_list = tube_list[::-1]
+#print(f'tube_list: {tube_list}')
+tube = zeros(ugrid.n_cells, dtype=int)
+slice = zeros(ugrid.n_cells, dtype=int)
+#c = 0
+for n in tube_list:
+    is_zero = tube==0
+    tube[is_zero] += tubes[n, is_zero]
+    new_slice = slices[n, is_zero]
+    #new_slice -= nanmin(new_slice)
+    #print(f'min, max, c: {new_slice.min()}, {slice.max()}, {c}')
+    ugrid[f'new_slice_{n}'] = zeros(ugrid.n_cells)
+    ugrid[f'new_slice_{n}'][is_zero] = new_slice
+    slice[is_zero] += new_slice - npmin(new_slice[nonzero(new_slice)]) + npmax(slice)
+    #c = slice.max()
+ugrid[f'tube'] = tube
+ugrid[f'slice'] = slice
+
 
 #ugrid['slice'] = idx
 

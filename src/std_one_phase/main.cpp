2e34b31800bd1d91ec45290af3e8efb786304234
eje74 <esje@norceresearch.no>
2024-01-26 15:38:58 +0100

    Added a source term to the main file in std_one_phase. Included and setTag and getTag in nodes

--- src/std_one_phase/main.cpp
+++ src/std_one_phase/main.cpp
@@ -95,7 +95,6 @@ void applySolidFluidBoundary(LbField<DXQY> &f, const std::vector<std::vector<int
     f(0, qUnknown, nodeFluid) = f(0, qKnown, nodeWall);
   }
 }
-
 //                                SOLID-FLUID BOUNDARY
 //------------------------------------------------------------------------------------- SOLID-FLUID BOUNDARY
 template<typename DXQY>
@@ -155,13 +154,112 @@ int main()
   BndMpi<LT> mpiBoundary(vtklb, nodes, grid);
   // Set bulk nodes
   std::vector<int> bulkNodes = findBulkNodes(nodes);
-  // Set node tags
+  //                              Node tag attributes
+  //------------------------------------------------------------------------------------- nodetags attribute
+  //       0: solid
+  //       1: fluid phase 1
+  //       2: fluid phase 2 
+  //       3: pressure boundary' ghost node (treated as a solid node)
+  //       4: fluid-fluid interface
+  //       8: fluid-solid interface
+  //      16: pressure boundary
+  ScalarField tagsTmp(1, grid.size());
   vtklb.toAttribute("nodetags");
   for (int n=vtklb.beginNodeNo(); n < vtklb.endNodeNo(); ++n)
   {
     int tag = vtklb.getScalarAttribute<int>();
     nodes.setTag(tag, n); 
+    tagsTmp(0, n) = tag;
   }
+  //                              domain attributes
+  //------------------------------------------------------------------------------------- domain labels
+  ScalarField domains(1, grid.size());
+  vtklb.toAttribute("domains");
+  for (int n=vtklb.beginNodeNo(); n < vtklb.endNodeNo(); ++n)
+  {
+    int domainNumber = vtklb.getScalarAttribute<int>();
+    domains(0, n) = domainNumber;
+  }
+  //                              force indicator
+  //------------------------------------------------------------------------------------- force indicator
+  ScalarField forceOn(1, grid.size());
+  vtklb.toAttribute("force");
+  for (int n=vtklb.beginNodeNo(); n < vtklb.endNodeNo(); ++n)
+  {
+    int val = vtklb.getScalarAttribute<int>();
+    forceOn(0, n) = val;
+  }
+  //                              interior domains
+  //------------------------------------------------------------------------------------- interior domains
+  ScalarField interiorDomains(1, grid.size());
+  std::vector<int> interiorDomainsLabel(grid.size(), 0);
+  std::vector<lbBase_t> addMassSource(grid.size(), 0.0);
+  vtklb.toAttribute("interior_domains");
+  int localDomainLabelMax = 0;
+  for (int n=vtklb.beginNodeNo(); n < vtklb.endNodeNo(); ++n)
+  {
+    int val = vtklb.getScalarAttribute<int>();
+    interiorDomains(0, n) = val;
+    interiorDomainsLabel[n] = val;
+    if (nodes.isMyRank(n))
+      if (val > localDomainLabelMax)
+        localDomainLabelMax = val;
+  }
+  //                              setup communication for mass conservation
+  //------------------------------------------------------------------------------------- init mass conservation
+  int globalDomainLabelMax;
+  MPI_Allreduce(&localDomainLabelMax, &globalDomainLabelMax, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+  std::cout << "Rank: " << myRank << " local max domain label = " << localDomainLabelMax << "  global domain max = " << globalDomainLabelMax << std::endl;
+  // size of the interior of the "interior domains", that is, at
+  // the nodes where mass is added or subtracted.
+  std::vector<lbBase_t> massSourceScaleFactor(globalDomainLabelMax + 1, 0);
+  std::vector<lbBase_t> massChangeLocal(globalDomainLabelMax + 1, 0);
+  std::vector<lbBase_t> massChange(globalDomainLabelMax + 1, 0);
+  {
+    std::vector<lbBase_t> tmp(globalDomainLabelMax + 1, 0.0);
+    for (int n = 1; n < grid.size(); ++n)
+    {
+      if (nodes.isMyRank(n)) 
+      { 
+        int label = interiorDomainsLabel[n];
+        int tag = nodes.getTag(n);
+        if ( (label > 0) &&  (tag < 3) ) 
+        {
+          tmp[label] += 1;
+          addMassSource[n] = 1.0;
+        } 
+      }
+    }
+    // MPI Communicate number of mass sources per interior domain
+    MPI_Allreduce(tmp.data(), massSourceScaleFactor.data(), globalDomainLabelMax+1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD); 
+    for (int i = 1; i < globalDomainLabelMax + 1; ++i) 
+    {
+      if ( massSourceScaleFactor[i] == 0.0) {
+        std::cout << "ERROR: Interior domain " << i << " has no interior nodes!" << std::endl;
+        exit(1);
+      } 
+      massSourceScaleFactor[i] = 1.0/massSourceScaleFactor[i];
+    }
+  }
+  // if (myRank == 0) {
+  //   int i = 0;
+  //   for (auto val: massSourceScaleFactor){
+  //     std::cout << i << " " << val << std::endl;
+  //     i++;
+  //   }
+  // }
+ 
+  // Output<LT> outputTest(grid, bulkNodes, outputDir, myRank, nProcs);
+  // outputTest.add_file("geo");
+  // outputTest.add_scalar_variables(
+  //   {"tags", "domains", "force", "interior_domains"},
+  //   {tagsTmp, domains, forceOn, interiorDomains}
+  // );
+  // outputTest.write(0);
+  // MPI_Finalize();
+  
+  // return 0;
+
   //=====================================================================================
   //
   //                                  READ FROM INPUT
@@ -203,7 +301,6 @@ int main()
   //                                MACROSCOPIC FIELDS
   //
   //=====================================================================================
-
   //                                     Fluid Flow
   //===================================================================================== Fluid Flow
   //                                      Density
@@ -290,6 +387,21 @@ int main()
   {
     //   int rampTimesteps = 5000; //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////NB!    
     //   const lbBase_t ramp{ 0.5 * (1-std::cos(PI*std::min(i, rampTimesteps)/rampTimesteps)) };
+
+    //                                   Mass conservation
+    //------------------------------------------------------------------------------------- mass conservation
+    // Sett local to zeros
+    std::fill(massChangeLocal.begin(), massChangeLocal.end(), 0.0);
+    for (auto nodeNo: bulkNodes) {
+      const std::valarray<lbBase_t> fNode = f(0, nodeNo); 
+      const lbBase_t rhoNode = calcRho<LT>(fNode);
+      // Source term
+      int label = interiorDomainsLabel[nodeNo];
+      massChangeLocal[label] += 1.0 - rhoNode;
+    } 
+    MPI_Allreduce(massChangeLocal.data(), massChange.data(), massChange.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    // Sett local to zeros
+    std::fill(massChangeLocal.begin(), massChangeLocal.end(), 0.0);    
     //                                   Main calculation loop
     //------------------------------------------------------------------------------------- 
     for (auto nodeNo: bulkNodes) 
@@ -302,8 +414,15 @@ int main()
       const std::valarray<lbBase_t> fNode = f(0, nodeNo);     
       //                                      Macroscopic values
       //------------------------------------------------------------------------------------- Macroscopic values
-      const lbBase_t rhoNode = calcRho<LT>(fNode);
-      const auto velNode = calcVel<LT>(fNode, rhoNode, bodyForce(0, 0));      
+      lbBase_t rhoNode = calcRho<LT>(fNode);
+      // Source term
+      int label = interiorDomainsLabel[nodeNo];
+      lbBase_t qMassConservation = 0.9*2*massSourceScaleFactor[label]*massChange[label]*addMassSource[nodeNo];
+      // Add source term
+      rhoNode += 0.5*qMassConservation;
+      massChangeLocal[label] += 1.0 - rhoNode;
+      const std::valarray<lbBase_t> forceNode = bodyForce(0, 0)*forceOn(0, nodeNo);
+      const auto velNode = calcVel<LT>(fNode, rhoNode, forceNode);      
       //                            Save density and velocity for printing
       //------------------------------------------------------------------------------------- Save density and velocity for printing
       rho(0, nodeNo) = rhoNode;
@@ -312,25 +431,24 @@ int main()
       //------------------------------------------------------------------------------------- BGK-collision term
       const lbBase_t u2 = LT::dot(velNode, velNode);
       const std::valarray<lbBase_t> cu = LT::cDotAll(velNode);      
-  //     //const auto feqNode = calcfeq<LT>(rhoNode, u2, cu);
-  //     //const auto omegaBGKTot = newtonian.omegaBGK(tau, fNode, rhoNode, velNode, u2, cu, ForceField(0, nodeNo), Qfield(0, nodeNo));      
       const std::valarray<lbBase_t> omegaBGK = calcOmegaBGK<LT>(fNode, tau, rhoNode, u2, cu);            
       //                           Calculate the Guo-force correction
       //------------------------------------------------------------------------------------- Calculate the Guo-force correction
-      const lbBase_t uF = LT::dot(velNode, bodyForce(0, 0));
-      const std::valarray<lbBase_t> cF = LT::cDotAll(bodyForce(0, 0));
+      const lbBase_t uF = LT::dot(velNode, forceNode);
+      const std::valarray<lbBase_t> cF = LT::cDotAll(forceNode);
       const std::valarray<lbBase_t> deltaOmegaF = calcDeltaOmegaF<LT>(tau, cu, uF, cF);
-  //     //                           Calculate the mass-source correction
-  //     //------------------------------------------------------------------------------------- Calculate the mass-source correction
-  //     const std::valarray<lbBase_t> deltaOmegaQ0 = calcDeltaOmegaQ<LT>(tau, cu, u2, Qfield(0, nodeNo));
+      //                           Calculate the mass-source correction
+      //------------------------------------------------------------------------------------- Calculate the mass-source correction
+      const std::valarray<lbBase_t> deltaOmegaQ0 = calcDeltaOmegaQ<LT>(tau, cu, u2, qMassConservation);
       //                               Collision and propagation
       //------------------------------------------------------------------------------------- Collision and propagation
-      fTmp.propagateTo(0, nodeNo, fNode + omegaBGK + deltaOmegaF, grid);
-      // fTmp.propagateTo(0, nodeNo, fNode + omegaBGK + deltaOmegaF + deltaOmegaQ0, grid);
+      // fTmp.propagateTo(0, nodeNo, fNode + omegaBGK + deltaOmegaF, grid);
+      fTmp.propagateTo(0, nodeNo, fNode + omegaBGK + deltaOmegaF + deltaOmegaQ0, grid);
     } //------------------------------------------------------------------------------------- End for bulkNodes
     //                           Swap lb fields
     //------------------------------------------------------------------------------------- Swap lb fields
     f.swapData(fTmp);
+
     //=====================================================================================
     //
     //                               BOUNDARY CONDITIONS
@@ -355,6 +473,20 @@ int main()
       if (myRank==0) {
 	      std::cout << "PLOT AT ITERATION: " << i << std::endl;
       }
+      //                           Update mass source
+      //------------------------------------------------------------------------------------- Update mass source
+      MPI_Allreduce(massChangeLocal.data(), massChange.data(), massChange.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+      // Sett local to zeros
+      std::fill(massChangeLocal.begin(), massChangeLocal.end(), 0.0);
+      // Test
+      if (myRank == 0) {
+        massChange[0] = 0;
+        int cnt = 0;
+        for (auto m: massChange) 
+        {
+          std::cout << cnt << " " << m << std::endl;
+        } 
+      }      
    }  
   } //----------------------------------------------------------------------------------------  End for nIterations
   

b3fb737117da72e8d47976caa02ed69ed396336f
eje74 <esje@norceresearch.no>
2026-09-16 11:04:04 +0200

    inputfile work in progress

--- src/std_one_phase/PythonScripts/norce_hpc_workflow.py
+++ src/std_one_phase/PythonScripts/norce_hpc_workflow.py
@@ -3,31 +3,39 @@ import os
 import argparse
 import time
 from generate_geometry_mpi import generate_geometry_mpi
-from generate_geometry_mpi import write_input_file
+from generate_geometry_mpi import write_input_hpc
 
 p = argparse.ArgumentParser()
 p.add_argument("--outputpath", type=str, required=True)
 args = p.parse_args()
 
 outputpath = args.outputpath
+inputpath = r"./"
+datapath = r"/cluster/home/esje/github/BADChIMP-cpp/input/data/"
 
-print("outputpath = ", outputpath)
+filenamedata = r"GH_PoreSolid_400x400x400_SDF_PD"
+filename = datapath + filenamedata +  r".npy"
 
+# ------------------------------------------------------------------------ generate geometry
+pore = np.load(filename)
+geo = np.ones(pore.shape, dtype=np.int32)
+geo[pore>0] = 0
 
-path = "/cluster/home/esje/"
-lbpath = path + r"github/BADChIMP-cpp/"
-filename = lbpath + r"input/data/GH_PoreSolid_400x400x400_SDF_PD.npy"
+num_proc = (4,)*3
 
+nproc = generate_geometry_mpi(geo, num_proc, inputpath + r"input/mpi/")
 
-# pore = np.load(filename)
+print("NUM PROC = ", num_proc)
 
-# # ------------------------------------------------------------------------ generate geometry
-# geo = np.ones(pore.shape, dtype=np.int32)
-# geo[pore>0] = 0
-
-# num_proc = (4,)*3
-
-# nproc = generate_geometry_mpi(geo, num_proc, lbpath + r"input/mpi/")
+write_input_hpc(
+    inputpath,
+    5000,
+    500,
+    0.8,
+    1e-6,
+    outputpath,
+    filenamedata
+)
 
 # write_input_file(
 #     lbpath,

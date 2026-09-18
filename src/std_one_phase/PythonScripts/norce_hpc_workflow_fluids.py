70d692bfef67b113a60692a28a39ca142a0178cf
eje74 <esje@norceresearch.no>
2026-09-18 11:35:50 +0200

    Wrote the python workflow file for phase permeability runs

---
+++ src/std_one_phase/PythonScripts/norce_hpc_workflow_fluids.py
@@ -0,0 +1,90 @@
+import numpy as np
+import os
+import argparse
+import time
+from generate_geometry_mpi import generate_geometry_mpi
+from generate_geometry_mpi import write_input_hpc
+
+p = argparse.ArgumentParser()
+p.add_argument("--outputpath", type=str, required=True)
+p.add_argument("--solidfilename", type=str, required=True)
+p.add_argument("--fluidfilename", type=str, required=True)
+args = p.parse_args()
+
+outputpath = args.outputpath
+solidfilename = args.solidfilename
+fluidfilename = args.fluidfilename
+inputpath = r"./"
+datapath = r"/cluster/home/esje/github/BADChIMP-cpp/input/data/"
+
+
+# MPI setup
+# Initial regular composition of geo-array
+num_proc = (4,)*3
+
+
+# ------------------------------------------------------------------------ generate geometry
+pore = np.load(
+    datapath + solidfilename +  r".npy")
+
+fluid = np.load(
+    datapath + fluidfilename +  r".npy")
+
+geo = np.ones(
+    pore.shape, 
+    dtype=np.int32)
+
+# -------------------------------------------------------------------- Wetting phase
+geo[fluid<=0] = 0
+geo[pore>0] = 0
+
+basefilename = fluidfilename + "_W"
+# nproc is the actually number of processors used
+#  sbatch reads the from the ntasks_datafile
+nproc = generate_geometry_mpi(
+    geo, 
+    num_proc, 
+    inputpath + r"input/mpi/", 
+    vtklbfilename=basefilename
+    )
+
+with open(outputpath + "ntasks" + basefilename + ".txt", "w") as f:
+    f.write(str(nproc) + "\n")
+
+write_input_hpc(
+    inputpath,
+    20000,
+    500,
+    0.8,
+    1e-6,
+    outputpath,
+    basefilename
+)
+
+# -------------------------------------------------------------------- Non-wetting phase
+geo[:] = 1
+geo[fluid>0] = 0
+geo[pore>0] = 0
+
+basefilename = fluidfilename + "_NW"
+# nproc is the actually number of processors used
+#  sbatch reads the from the ntasks_datafile
+nproc = generate_geometry_mpi(
+    geo, 
+    num_proc, 
+    inputpath + r"input/mpi/", 
+    vtklbfilename=basefilename
+    )
+
+with open(outputpath + "ntasks" + basefilename + ".txt", "w") as f:
+    f.write(str(nproc) + "\n")
+
+write_input_hpc(
+    inputpath,
+    20000,
+    500,
+    0.8,
+    1e-6,
+    outputpath,
+    basefilename
+)
